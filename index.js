7316857df03c4529d64dc86721d89e987ec7accd
Benjamin Cook <benjamincook1313@gmail.com>
2023-03-09 11:39:45 -0700

    added post

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 
 const todos = document.getElementById("todos");
+const todoForm = document.getElementById("add");
+const item = document.querySelector("#newTodo");
+
+function displayItems(obj){
+  const listItem = document.createElement("li");
+  listItem.textContent = obj.item;
+  todos.appendChild(listItem);
+}
+
+function clear(){
+  todos.innerHTML = '';
+}
 
 function getTodos(){
-  axios.get("http://localhost:5051/api/todos").then((result) => {
-    // console.log(result.data);
-    result.data.forEach(item => {
-      const listItem = document.createElement("li");
-      listItem.textContent = item;
-      todos.appendChild(listItem);
-    });
+  clear();
+  axios.get("http://localhost:5051/api/todo").then((result) => {
+    result.data.forEach(todo => displayItems(todo));
   }).catch(err => console.log({err, message: "Problem getting todos"}));
 };
 
 function addTodo(e){
-  
+  e.preventDefault();
+  clear();
+  axios.post("http://localhost:5051/api/todo", {item: item.value}).then(result => {
+    result.data.forEach(todo => displayItems(todo));
+  }).catch(err => console.log(err));
 };
 
 getTodos();
+todoForm.addEventListener("submit", addTodo);
--- server/server.js
+++ server/server.js
@@ -6,22 +6,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const todos = ["wash dishes", "vacuum", "take out garbage", "walk the dog", "grocery shopping"];
+const { getTodos, addTodo } = require("./ctrl");
 
-app.get('/api/todos', (req, res) => {
-  res.status(200).send(todos);
-});
-
-app.post("/api/todos", (req, res) => {
-  const { todo } = req.body;
-
-  if(!todo){
-    res.sendStatus(400);
-  }else{
-    todos.push(todo);
-    res.status(200).send(todos);
-  }
-});
+app.get('/api/todo', getTodos);
+app.post("/api/todo", addTodo);
 
 
 const PORT = 5051;
